aa259bb00db9f2df43457180bb966f0028ff8baa
Dan Nelson <dan@cybertooth.io>
2018-11-30 00:44:37 -0700

    #7 rolling for rc.1

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,6 @@ module ErmahgerdRailsApiCognito
     # Ermahgerd Configuration
     # ------------------------------------------------------------------------------------------------------------------
 
-    config.version = '0.0.1-rc.0'
+    config.version = '0.0.1-rc.1'
   end
 end
